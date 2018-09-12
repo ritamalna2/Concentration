358787d537ebdaafce22355bcb4c688ad39f68d7
Kiss, Rita Szilvia <i347970@c02r55rlg8wp.dhcp.budl.sap.corp>
2018-09-12 12:41:30 +0200

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Kiss, Rita Szilvia on 2018. 09. 12..
+//  Copyright © 2018. Kiss, Rita Szilvia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
